68886da48af81e6c1941a85a7fa32a0e9c3a1342
Johnshandsable <Johnshandsable@outlook.com>
2021-04-08 10:28:42 -0500

    almost fixed chat feature

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ require("dotenv").config();
 
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST"],
+  },
+});
 
 const sessionMiddleware = require("./modules/session-middleware");
 const passport = require("./strategies/user.strategy");
