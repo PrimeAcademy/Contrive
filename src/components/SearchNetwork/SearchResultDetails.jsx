874a6925b4707a3da43b27468325ffe4afc223f7
McKynlee Westman <mckynlee.westman@gmail.com>
2021-04-14 17:21:56 -0500

    Routing to vendor profile when vendor info clicked in search results

--- src/components/SearchNetwork/SearchResultDetails.jsx
+++ src/components/SearchNetwork/SearchResultDetails.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Box, Grid } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 import './searchNetwork.css';
 
 function SearchResultDetails({ vendor }) {
-  console.log('====================================');
   console.log('vendor:', vendor);
-  console.log('====================================');
+
+  const history = useHistory();
+
+  function goToVendor(vendorUserId) {
+    console.log('====================================');
+    console.log('vendorUserId:', vendorUserId);
+    console.log('====================================');
+    history.push(`/vendor/${vendorUserId}`)
+  }
 
   return (
 
     <Grid item xs={5}>
-      <Box mt={3} mb={3}>
+      <Box mt={3} mb={3} onClick={() => goToVendor(vendor.vendorUserId)}>
         <img src={vendor.profilePic} alt={vendor.companyName} className="vendorProfilePic" />
         {/* ToDo: Add Vendor Rating (Stretch) here */}
         {/* <Box>{vendor.rating}</Box> */}
